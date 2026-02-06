a94600364441faef863bf2700efdb7e65fe44003
Anjali Singh <singhanjali33375@gmail.com>
2026-02-06 14:36:26 +0530

    Create server.js

---
+++ prometheus/app/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Monitoring Demo App Running");
+});
+
+app.listen(8080, () => {
+  console.log("App running on port 8080");
+});
